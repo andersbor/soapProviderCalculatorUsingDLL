abc7e8041076dd908e1e2e7e57e949e6a1f3f635
andersbor <anbo@easj.dk>
2017-10-03 08:45:12 +0200

    Add project files.

---
+++ SoapCalculatorUsingDLL/ICalculatorService.cs
@@ -0,0 +1,31 @@
+﻿using System.ServiceModel;
+
+namespace SoapCalculatorUsingDLL
+{
+    [ServiceContract]
+    public interface ICalculatorService
+    {
+        [OperationContract]
+        int Add(int a, int b);
+        [OperationContract]
+        int Subtract(int a, int b);
+        [OperationContract]
+        int Multiply(int a, int b);
+        [OperationContract]
+        int Divide(int a, int b);
+
+        [OperationContract]
+        // SOAP does not accept overloaded method names
+        double AddDoubles(double a, double b);
+
+        [OperationContract(Name = "SubtractDoubles")]
+        // Another way to avoid overloaded methods names in SOAP
+        double Subtract(double a, double b);
+
+        [OperationContract]
+        double MultiplyDoubles(double a, double b);
+
+        [OperationContract]
+        double DivideDoubles(double a, double b);
+    }
+}
---
+++ SoapCalculatorUsingDLL/Service1.svc.cs
@@ -0,0 +1,49 @@
+﻿using CalculatorComponent;
+
+namespace SoapCalculatorUsingDLL
+{
+    // must have a reference til CalculatorComponent
+    // Add Reference .... (not ServiceReference)
+    public class CalculatorService : ICalculatorService
+    {     
+        public int Add(int a, int b)
+        {
+            return Calculator.Add(a, b);
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return Calculator.Subtract(a, b);
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return Calculator.Multiply(a, b);
+        }
+
+        public int Divide(int a, int b)
+        {
+            return Calculator.Divide(a, b);
+        }
+
+        public double AddDoubles(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double MultiplyDoubles(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double DivideDoubles(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
